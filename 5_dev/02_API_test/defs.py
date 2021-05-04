91239fa5b365ef31dcbfcc2d7a60e1f86dd2f632
Sahenjit <sahenjit951@hotmail.co.uk>
2021-05-04 21:01:08 +0100

    test_change

--- 5_dev/02_API_test/defs.py
+++ 5_dev/02_API_test/defs.py
@@ -1,5 +1,5 @@
-API_KEY = "xxxx"
-ACCOUNT_ID = "xxxx"
+API_KEY = "c2d3779e25a27729651bff135000f4eb-85f3ccf83a144ad23e6c125b628b4401"
+ACCOUNT_ID = "101-004-19105515-001"
 OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
 
 SECURE_HEADER = {
