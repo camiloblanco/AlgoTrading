80b2c040b091bee64d21c4448b41a3ce7f0dafa6
Sahenjit <sahenjit951@hotmail.co.uk>
2021-05-15 17:13:17 +0100

    Update Oanda_Manager.py

--- 5_dev/04_White_Robot/Oanda_Manager.py
+++ 5_dev/04_White_Robot/Oanda_Manager.py
@@ -11,23 +11,23 @@ class Oanda_Manager():
         self.API_KEY = Account_details.iloc[:, 1]
         self.Account_ID = Account_details.iloc[:, 2]
         self.Oanda_URL = Account_details.iloc[:, 3]
-        self.Bearer = {
-            'Authorization': f'Bearer {self.API_KEY}'
-        }
+        self.Header = {'Authorization': f'Bearer {self.API_KEY}'}
+
 
     def get_account_instrument(self, instrument_type):
-        instrument_list = f"{self.Oanda_URL}/accounts/{self.Account_ID}/instruments?instruments={instrument_type}"
-        print(instrument_list)
-        df = pd.DataFrame.from_dict(instrument_list)
+        request = f"{self.Oanda_URL}/accounts/{self.Account_ID}/instruments?instruments={instrument_type}"
+        print(request)
+        #Instrument_list = self.Session.get(request, params=None, headers=self.Header)
+        df = pd.DataFrame.from_dict(Instrument_list)
         return df[['name', 'type', 'displayName', 'pipLocation', 'marginRate']]
 
 
 
 
-def get_account_instrument(self, instrument_type):
-    pass
 
 
 if __name__ == "__main__":
     O_M = Oanda_Manager()
-    O_M.get_account_instrument("SPX500_USD")
+    print(f"{O_M.Oanda_URL}/accounts/{O_M.Account_ID}/instruments")
+
+    #O_M.get_account_instrument("SPX500_USD")
