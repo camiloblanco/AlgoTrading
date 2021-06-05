356abd658a70960c3b84aa6c5f6d1f7f21627716
Sahenjit <sahenjit951@hotmail.co.uk>
2021-06-05 11:14:10 +0100

    White_Strat_Update

--- 5_dev/04_White_Robot/White_Strategy.py
+++ 5_dev/04_White_Robot/White_Strategy.py
@@ -6,11 +6,22 @@ from scipy.stats import linregress
 class White_Strategy():
 
     def __init__(self, csv_filename):
-        self.series = pd.read_csv(csv_filename)
+        self.Asset_signals = pd.read_csv(csv_filename)
 
 
     def moving_avg_strategy(self):
-        pass
+        self.Asset_signals['SMA_14 > SMA_21'] = np.where((self.Asset_signals['SMA_14'] > self.Asset_signals['SMA_21']) &
+                                                         (self.Asset_signals['SMA_14'].shift() < self.Asset_signals['SMA_21'].shift()), 1, 0)
+        self.Asset_signals['SMA>14 > SMA_40'] = np.where((self.Asset_signals['SMA_14'] > self.Asset_signals['SMA_40']) &
+                                                         (self.Asset_signals['SMA_14'].shift() < self.Asset_signals['SMA_40'].shift()), 1, 0)
+        self.Asset_signals['SMA_14 < SMA_21'] = np.where((self.Asset_signals['SMA_14'] < self.Asset_signals['SMA_21']) &
+                                                         (self.Asset_signals['SMA_14'].shift() > self.Asset_signals['SMA_21'].shift()), 1, 0)
+        self.Asset_signals['SMA_14 < SMA_40'] = np.where((self.Asset_signals['SMA_14'] < self.Asset_signals['SMA_40']) &
+                                                         (self.Asset_signals['SMA_14'].shift() > self.Asset_signals['SMA_40'].shift()), 1, 0)
+
+        return self.Asset_signals
+
+
 
 
 
